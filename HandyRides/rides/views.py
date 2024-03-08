f2c0ba20ad707de168004f3cb66c4d182ab8ea52
dbotros3 <98500849+dbotros3@users.noreply.github.com>
2024-03-08 15:13:11 -0500

    Update views.py

--- HandyRides/rides/views.py
+++ HandyRides/rides/views.py
@@ -9,7 +9,24 @@ from .forms import RideForm
 
 from .forms import RideForm, NewRideForm
 
+import os
+
+from transformers import pipeline
+
 # Create your views here.
+
+
+def ai_interaction(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input')
+        rides_data = Person.objects.all()
+        system_message = f"You are trying to help folks get rides based on the data you have in your database: {rides:data}"
+        prompt = f"{system_message} {user_input} AI:"
+        generator = pipeline("text-generation", model="openai-community/gpt2")
+        ai_text = generator(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
+        final_ai_text = ai_text.split("AI:")[-1].strip()
+        return render(request, 'index_view.html', {'ai_text':final_ai_text})
+        return render(request, 'index_view.html')
 
 
 def index(request):
